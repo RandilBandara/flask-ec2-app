b1656717b3ede0436e55eb6e28a99a48e6b97edb
RandilBandara <randillbandara@gmail.com>
2025-07-16 13:27:19 +0530

    Update app.py and add test_app

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-≈from flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello from EC2 Flask App!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
